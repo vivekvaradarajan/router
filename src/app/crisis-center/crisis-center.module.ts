488a759170f105222057e861f1198b856d1f2267
Ran Gong <ran.gong@slalom.com>
2018-09-10 23:06:24 -0400

    create survey-answer set class

--- src/app/crisis-center/crisis-center.module.ts
+++ src/app/crisis-center/crisis-center.module.ts
@@ -13,6 +13,44 @@ import { CrisisCenterHomeComponent } from './crisis-center-home.component';
 import { CrisisDetailComponent }     from './crisis-detail.component';
 
 import { CrisisCenterRoutingModule } from './crisis-center-routing.module';
+export class AnswerSet
+{
+    SurveyName:string;
+    SectionSet:SectionSet[];
+    constructor(SurveyName:string,
+      SectionSet:SectionSet[]){
+            this.SurveyName = SurveyName;
+            this.SectionSet = SectionSet;
+        }
+}
+export class SectionSet{
+  Title:string;
+  Order:number;
+  Prompts:PromptSet[];
+  constructor(Title:string,Order:number,Prompts:PromptSet[]){
+    this.Title = Title;
+    this.Order= Order;
+    this.Prompts= Prompts;
+  }
+}
+export class PromptSet{
+  Text:string;
+  ResponseSet:ResponseSet[];
+  constructor(Text:string,ResponseSet:ResponseSet[]){
+    this.Text = Text;
+    this.ResponseSet = ResponseSet;
+  }
+}
+export class ResponseSet{
+  Label:string;
+  Value:string;
+  Type:string;
+  constructor(Label:string,Value:string,Type:string){
+    this.Label = Label;
+    this.Value = Value;
+    this.Type = Type;
+  }
+}
 
 @NgModule({
   imports: [
@@ -31,4 +69,7 @@ import { CrisisCenterRoutingModule } from './crisis-center-routing.module';
     CrisisService
   ]
 })
-export class CrisisCenterModule {}
+export class CrisisCenterModule {
+
+  
+}
