eb30049dcb33c53e81c427066a73d9d19f42aeb5
Ran Gong <ran.gong@slalom.com>
2018-10-21 23:04:00 -0400

    made the api call

--- src/app/add-risc/add-risc.component.ts
+++ src/app/add-risc/add-risc.component.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Patient } from '../survey-center/patient';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {surveyService} from '../survey-center/survey.service';
 
 @Component({
   selector: 'app-add-risc',
   templateUrl: './add-risc.component.html',
-  styleUrls: ['./add-risc.component.css']
+  styleUrls: ['./add-risc.component.css'],
+  providers: [surveyService]
 })
+
+
 export class AddRiscComponent implements OnInit {
   patient:Patient;
 
-  constructor() { }
+  constructor(private service: surveyService) { }
 
   ngOnInit() {
     this.patient = new Patient(0,"","","","","");
   }
 
+  savePatient(patient:Patient){
+    this.service.savePatient(patient)
+    .subscribe(patient => console.log('result',patient));
+  }
+
+  
 }
